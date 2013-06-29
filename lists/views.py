ee31b9df71dc52d0eb4369dca5ce2afa2da33c6a
jawahar <jawahar.malhotra@gmail.com>
2013-06-28 19:07:21 -0700

    basic view now returns minimal html

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
-def home_page():
-    pass
+from django.http import HttpResponse
+
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
+
 
 
