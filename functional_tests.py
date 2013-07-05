254bae3f53ed4e61a82f50da0f1cb38a372555b1
jawahar <jawahar.malhotra@gmail.com>
2013-07-05 12:11:31 -0700

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -9,16 +10,29 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # get the homepage of the app
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        # check header of page
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # enter some input into the page
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        # get the table from the response
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers') for row in rows)
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main()
 
 
-#browser = webdriver.Firefox()
-#browser.get('http://localhost:8000')
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
 
-#assert 'Django' in browser.title
-#browser.quit()
+assert 'Django' in browser.title
+browser.quit()
