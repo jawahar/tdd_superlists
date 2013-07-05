6776ff26efd1033aac7dcc18ede407c95e124ad5
jawahar <jawahar.malhotra@gmail.com>
2013-07-05 12:36:37 -0700

    refactor home page view to use a template; changed test to use template and compare response to expected output from template

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,10 @@
 # Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
+
 
 
 
